692246c395f96b0b1d3fbcc34ad3a6bac112eec9
DolphaGo <adamdoha@naver.com>
2021-05-13 00:02:15 +0900

    기본 Q-Type 활용

--- src/test/java/com/example/querydsl/QuerydslBasicTest.java
+++ src/test/java/com/example/querydsl/QuerydslBasicTest.java
@@ -1,5 +1,6 @@
 package com.example.querydsl;
 
+import static com.example.querydsl.entity.QMember.member;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import javax.persistence.EntityManager;
@@ -59,12 +60,19 @@ public class QuerydslBasicTest {
     @Test
     void startQuerydsl() {
 //        JPAQueryFactory queryFactory = new JPAQueryFactory(em); // 대신 필드로 가져가자.
-        QMember m = new QMember("m"); // 어떤 Qmember일지 구분하는 역할. QMember.member를 사용할 수도 있다.
+
+//        같은 테이블을 조인하는 경우엔 alias가 달라야 하니 그때만 이렇게 선언해서 사용하면 된다.
+//        QMember m = new QMember("m"); // 어떤 Qmember일지 구분하는 역할.
+//
+//        QType에 미리 만들어진 QMember.member를 사용할 수도 있다. 이 alias는 기본적으로 "member1"로 되어있다.
+//        실행된 JPQL을 확인하면 alias를 기준으로 쿼리가 작성되는 것을 확인할 수 있다.
+//
+//        QMember m = QMember.member; // 대신 static import로 코드를 더 간략하고 명료하게 할 수도 있다.
 
         Member findMember = queryFactory
-                .select(m)
-                .from(m)
-                .where(m.username.eq("member1")) // prepareStatement로 자동으로 Parameter binding이 가능하다.
+                .select(member)
+                .from(member)
+                .where(member.username.eq("member1")) // prepareStatement로 자동으로 Parameter binding이 가능하다.
                 .fetchOne();
 
         assertEquals("member1", findMember.getUsername());
